2a1f5debaa437fbea56ea9abdf4c0106358556fa
adammkeane <adamkeane.edu@gmail.com>
2023-03-09 18:38:39 +0000

    Creat skeleton post detail view, template and url link

--- workapp/urls.py
+++ workapp/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
+import random
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='bulletin'),
-
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
